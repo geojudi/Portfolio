1843024f4d31f5474ef1bab470604f91254ca05f
Judi Sclafani <32069911+geojudi@users.noreply.github.com>
2023-12-14 11:30:23 -0700

    Update PBDBtoAbundMatrices.r

--- PBDBtoAbundMatrices.r
+++ PBDBtoAbundMatrices.r
@@ -1,10 +1,16 @@
-# Convert PALEOBIOLOGY DATABASE DOWNLOAD TO ABUNDANCE MATRICES
+##################################################################################################################################
+############################### CONVERT PALEOBIOLOGY DATABASE DOWNLOAD TO ABUNDANCE MATRICES #####################################
+##################################################################################################################################
 
-# This script will generate abundance matrices for all collections in a Paleobiology Databse (PBDB) download file. The download file must contain the abundance, time bins, and environment fields as collections are split by collection number, time, and environment. For the program to run correctly, the name of the file should be  pbdbdata-occs.csv and the column names should be unaltered from those output by the PBDB. Data should be downloaded without comments or comment columns should be removed in Excel because punctuation in the comments can offset the column alignment in the .csv file. 
+# This script will generate a abundance matrix containing data for all collections in a Paleobiology Databse (PBDB) download file. It will then split that matrix into separate .csv files each containing the abundance data for only one collection.
+
+# requirements: The download file must contain the abundance, time bins, and environment fields as collections are split by collection number, time, and environment. For the program to run correctly, the name of the file should be  pbdbdata-occs.csv and the column names should be unaltered from those output by the PBDB. Data should be downloaded without comments or comment columns should be removed in Excel because punctuation in the comments can offset the column alignment in the .csv file. 
+
+# recommendation: create a new folder to contain the output .csv files and set that as the working directory. A large input file typically contains hundreds of collections. Dedicating a clean folder for the outputs best facilitates organization for future analyses.
 
 # input: a .csv file downloaded from the PBDB. This file should be named pbdbdata-occs.csv
 
-# output: multiple .csv files, each of which contains an abundance matrix. The number of output files is determined by number of collections in downloaded file and the number of time bins and depositional environments in each unique collection
+# output: multiple .csv files, an abundance matrix for each collection in the inpur file. The number of output files is determined by number of collections in downloaded file and the number of time bins and depositional environments in each unique collection
 
 #### NOTE BEFORE RUNNING ANALYSIS: This is set up to run all taxonomic classes and orders designated as suspension feeders. To analyze a different trophic level, the function RemoveNonSuspensionFeeders will need to be adjusted. 
 
@@ -60,19 +66,21 @@ GenerateAbundanceMatrix <- function(pbdbData){
 #################################################################################################################################
 
 
-## CREATE MATRICES OF ABUNDANCE DATA AND COLLECTION INFO ##
+#################################################################################################################################
+######################################## PERFORM ABUNDANCE MATRIX TRANSFORMATIONS ###############################################
+#################################################################################################################################
 
-# set data file to read in data
+# read in data
 InputFile <- 'pbdbdata-occs.csv'
 pbdbData <- read.csv(file=InputFile, as.is=TRUE)
 
+########################## Perform data quality culling procedures ####################################
 # remove any data that does not have an associated time bin or environment
 pbdbData <- pbdbData[which(as.vector(pbdbData$time_bin)!=""), ]
 pbdbData <- pbdbData[which(as.vector(pbdbData$environment)!=""), ]
 
 # remove taxa that are not in the list of suspension feeders
 pbdbData <- RemoveNonSuspensionFeeders(pbdbData)
-
 
 ## remove collections with problematic abundance values
 
@@ -85,35 +93,35 @@ collectionstoRemove <- pbdbData$collection_no[which(pbdbData$abund_value %in% un
 # remove an entire collection if it contains any non-numeric abundance values
 indices <- which(pbdbData$collection_no %in% collectionstoRemove)
 pbdbData <- pbdbData[-indices, ]
+#######################################################################################################
 
-
-## transform data download to abundance matrix
+######################### Transform data download to abundance matrix #################################
 AbundanceMatrix <- GenerateAbundanceMatrix(pbdbData)
 collectionSize <- rowSums(AbundanceMatrix)
+#######################################################################################################
 
-# cull abundance matrix to include only collections of sizes larger than 5
-# this determination is based on the summary statistics and to improve statistical robustness
+######################### Statistical culling #########################################################
+# Include only collections of sizes larger than 5. This determination was based on the summary statistics. 
 indices <- which(collectionSize > 5)
 AbundanceMatrix <- AbundanceMatrix[indices, ] 
 
-# cull abundance matrix to remove collections that have fewer than 2 genera
+# Remove collections that have fewer than 2 genera
 numGenera <- c()
 for(i in 1:nrow(AbundanceMatrix)){
     numGenera[i] <- length(which(AbundanceMatrix[i, ]!=0))
 }
-
 indices <- which(numGenera >1)
 AbundanceMatrix <- AbundanceMatrix[indices, ]
 
-# remove any genera that, after the culling, have an abundance of zero 
+# remove any genera that, after the collection culling, have an abundance of zero 
 AbundanceMatrix <- AbundanceMatrix[ ,which(colSums(AbundanceMatrix)!=0)]
+#######################################################################################################
 
-## make collection info data table
-
+######################### Build collection context info data table ####################################
 # make a list of unique collections
 collections <- unique(pbdbData$collection_no)
 
-# set and fill desired information fields
+# set and fill desired information fields for collection context
 age <- c()
 paleolat <- c()
 paleolong <- c()
@@ -138,18 +146,17 @@ for(i in 1:length(collections)){
 	referenceno[i] <- as.vector(unique(pbdbData$reference_no[which(pbdbData$collection_no==collections[i])]))
 }
 
-# combine fields generated above, collection number, and collection size into data frame
-CollectionInfo <- cbind(collections, age, paleolat, paleolong, environment, country, state, plate, lithification, lithology, referenceno, collectionSize)
+# combine fields generated above, collection number, and collection size into a data frame
+CollectionInfo <- as.data.frame(collections, age, paleolat, paleolong, environment, country, state, plate, lithification, lithology, referenceno, collectionSize)
 colnames(CollectionInfo) <- c("collectionNo", "age", "paleolat", "paleolong", "environment", "country", "state", "plate", "lithification", "lithology", "ReferenceNo", "collectionSize")
 
-# transform the CollectionInfo matrix to a dataframe so that dollar notation can be used later
-CollectionInfo <- as.data.frame(CollectionInfo)
-
-# this is to make sure the collection info and abundance matrices contain the same collections
+# make sure the collection info and abundance matrices contain the same collections
 CollectionInfo <- CollectionInfo[which(CollectionInfo$collectionNo %in% rownames(AbundanceMatrix)), ]
 CollectionInfo <- droplevels(CollectionInfo)
+#######################################################################################################
 
-## write culled paleobiodb data, abundance matrix, collection info matrix
+##################### Save generated data objects for use outside of R ################################
+# Write culled paleobiodb data, abundance matrix, collection info matrix
 
 write.csv(pbdbData, file='culledPBDB.csv')
 write.csv(AbundanceMatrix, file="Abundances.csv")
